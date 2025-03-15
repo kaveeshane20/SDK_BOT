18cdc233330932a3d0ce00e939d0f8bdab5d4911
Kaveesha <120721339+kaveeshane20@users.noreply.github.com>
2025-03-15 21:55:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'SDK',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "SDK Bot",
+  botOwner: 'Kaveesha Nethum', // Your name
+  ownerNumber: '1234567889', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
